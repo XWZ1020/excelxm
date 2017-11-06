0b67f90f91bd9935192b53d329717c95c9beb71a
shixlei <896539265@qq.com>
2017-11-06 17:44:11 +0800

    选择上传文件
    可以选择从本地上传excel

--- duqu.php
+++ duqu.php
@@ -1,27 +1,47 @@
 <?php
-require_once "PHPExcel/IOFactory.php";
 
+require_once 'PHPExcel\PHPExcel.php'; 
+require_once 'PHPExcel\PHPExcel\IOFactory.php';
+require_once 'PHPExcel\PHPExcel\Reader\Excel5.php'; 
+require_once 'PHPExcel\PHPExcel\Reader\Excel2007.php'; 
+
+//上传文件
+//print_r($_FILES);die;
+$filename=$_FILES['myFile']['name'];
+$type=$_FILES['myFile']['type'];
+$tmp_name=$_FILES['myFile']['tmp_name'];
+$size=$_FILES['myFile']['size'];
+$error=$_FILES['myFile']['error'];
+move_uploaded_file($tmp_name,$filename);
+
+$objPHPExcel = new PHPExcel();
 $conn = mysqli_connect('127.0.0.1','root','','kebiao');
 $reader = PHPExcel_IOFactory::createReader('Excel2007'); //设置以Excel2007格式
-$PHPExcel = $reader->load("kebiao.xlsx"); // 载入excel文件   选择本地文件上传导入
+$PHPExcel = $reader->load($filename); // 载入上传的excel文件   选择本地文件上传导入
 $sheet = $PHPExcel->getSheet(0); // 读取第一個工作表
 $highestRow = $sheet->getHighestRow(); // 取得总行数
 $highestColumm = $sheet->getHighestColumn(); // 取得总列数
 $conn->query("set names utf8");//设置编码
 
+
+
 /** 循环读取并插入每个单元格的数据 */
-for ($row = 5; $row <= $highestRow ; $row++){//行数是以第2行开始
+for ($row = 5; $row <= $highestRow ; $row++){//行数是以第5行开始
     for ($column = 'A'; $column < $highestColumm; $column++) {//列数是以A列开始
+        
         $dataset[] = $sheet->getCell($column.$row)->getValue();//单元格值放进数组
         //echo $column.$row.":".$sheet->getCell($column.$row)->getValue()."<br />";
     }
     
-    $sqli="INSERT into kecheng (XINGMING,ROOM,WEEK,CLASS) values('$dataset[5]','$dataset[9]','$dataset[11]','$dataset[12]')";
+    $sqli="INSERT into kecheng (XINGMING,ROOM,WEEK,CLASS) values('$dataset[5]','$dataset[9]','$dataset[11]','$dataset[4]')";
     $conn ->query($sqli);
-    for($i = 0;$i < count($dataset);$i++){
-        echo $dataset[$i].",";//输出每个单元格值
-    }
     
+    // for($i = 0;$i < count($dataset);$i++){
+    //     echo $dataset[$i].",";//输出每个单元格值
+    // }
+    // 
+    // 打印错误
+    //echo "Error: " . $sqli . "<br>" . $conn->error;
    
     echo "<br>";
     unset($dataset);//重置数组
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>上传文件</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<form action="duqu.php" method="post" enctype="multipart/form-data">
+<p>请选择上传的文件：
+<input type="file" name="myFile"><br>
+<input type="submit" name="上传文件">
+</form>
+</body>
+</html>
