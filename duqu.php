060474516078c70485662c9b1a15cae4147ecbfa
shixlei <896539265@qq.com>
2017-11-14 16:57:09 +0800

    完成
    基本完成，但效率低下，还得修改

--- duqu.php
+++ duqu.php
@@ -19,33 +19,31 @@ $error=$_FILES['myFile']['error'];
 move_uploaded_file($tmp_name,$filename);
 
 //链接mysql   自动建库建表
-// $conn = mysqli_connect($servername,$username,$password);
-// mysqli_set_charset($conn,'utf8_general_ci');//不写这句话的话会造成乱码
-// $sql1 = "CREATE DATABASE kebiao CHARACTER SET 'utf8' COLLATE 'utf8_general_ci'";
-// if(mysqli_query($conn,$sql1)){
-//     $dbname = "kebiao";
-// }else{
-//     echo "数据库创建失败:" . mysqli_error($conn);
-// }
+$conn = mysqli_connect($servername,$username,$password);
+mysqli_set_charset($conn,'utf8_general_ci');//不写这句话的话会造成乱码
+$sql1 = "CREATE DATABASE kebiao CHARACTER SET 'utf8' COLLATE 'utf8_general_ci'";
+if(mysqli_query($conn,$sql1)){
+    $dbname = "kebiao";
+}else{
+    echo "数据库创建失败:" . mysqli_error($conn);
+}
 
-// $conn = mysqli_connect($servername,$username,$password,$dbname);
-// mysqli_set_charset($conn,'utf8_general_ci');//不写这句话的话会造成乱码
-// if(!$conn){
-// die("连接失败：".mysqli_connect_error());
-// }
-// //创建数据库(不能使用name，这是关键字)
-// $sql = "CREATE TABLE kecheng (
-// ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-// XINGMING VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-// ROOM INT(4) NOT NULL,
-// WEEK VARCHAR(50) NOT NULL,
-// CLASS VARCHAR(10) NOT NULL
-// );";
-// if(!(mysqli_query($conn,$sql))){
-// echo "数据表创建失败:" . mysqli_error($conn);
-// }
-
-
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+mysqli_set_charset($conn,'utf8_general_ci');//不写这句话的话会造成乱码
+if(!$conn){
+die("连接失败：".mysqli_connect_error());
+}
+//创建数据库(不能使用name，这是关键字)
+$sql = "CREATE TABLE kecheng (
+ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+XINGMING VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+ROOM INT(4) NOT NULL,
+WEEK VARCHAR(50) NOT NULL,
+JIE VARCHAR(10) NOT NULL
+);";
+if(!(mysqli_query($conn,$sql))){
+echo "数据表创建失败:" . mysqli_error($conn);
+}
 
 $objPHPExcel = new PHPExcel();
 $conn = mysqli_connect('127.0.0.1','root','','kebiao');
@@ -56,8 +54,6 @@ $sheet = $PHPExcel->getSheet(0); // 读取第一個工作表
 $highestRow = $sheet->getHighestRow(); // 取得总行数
 $highestColumm = $sheet->getHighestColumn(); // 取得总列数
 $conn->query("set names utf8");//设置编码
-
-
 
 /** 循环读取并插入每个单元格的数据 */
 for ($row = 5; $row <= $highestRow ; $row++){//行数是以第5行开始
@@ -102,7 +98,7 @@ for ($row = 5; $row <= $highestRow ; $row++){//行数是以第5行开始
         }
     }
     
-    $sqli="INSERT into kecheng (XINGMING,ROOM,WEEK,CLASS) values('$dataset[5]','$dataset[9]','$dataset[14]','$dataset[13]')";
+    $sqli="INSERT into kecheng (XINGMING,ROOM,WEEK,JIE) values('$dataset[5]','$dataset[9]','$dataset[14]','$dataset[13]')";
     $conn ->query($sqli);
     
     //打印
@@ -118,4 +114,5 @@ for ($row = 5; $row <= $highestRow ; $row++){//行数是以第5行开始
 
 }
 
+header("refresh:0;url=http://localhost:82/excelxm/export");
 ?>
--- export.php
+++ export.php
@@ -2,8 +2,18 @@
 	$dir=dirname(__FILE__);//查找当前脚本所在路径
 	require $dir."/PHPExcel/PHPExcel.php";//引入PHPExcel
 	require $dir."/db.php";//引入mysql操作类文件
+ 
+	require_once 'PHPExcel\PHPExcel\IOFactory.php';
+	require_once 'PHPExcel\PHPExcel\Reader\Excel5.php'; 
+	require_once 'PHPExcel\PHPExcel\Reader\Excel2007.php'; 
+
+	$servername = "localhost";
+	$username ="root";
+	$password = "";
+	$dbname="kebiao";
 	
 	$db = new db($phpexcel);//实例化db类
+
 	$objPHPExcel=new PHPExcel();//实例化phpexcel类   等同于创建一个excel
 	for ($i=1; $i <8 ; $i++) { 
 		if($i>1){
@@ -36,37 +46,104 @@
                 }
         $objSheet->setTitle("周".$weeknum);
 		$data=$db->getDataByWeek($weeknum);
-		//print_r($data);die;
-
-//输出需求表
+		//print_r($data);
+		
+		//输出需求表
 		$objSheet->setCellValue("A1","教室号")->setCellValue("B1","一二节")->setCellValue("C1","三四节")->setCellValue("D1","七八节")->setCellValue("E1","九十节")->setCellValue("F1","十一~十三节");
 		$j=2;
 		$jiaoshi=$db->getDataByROOM($ROOM);
+
 		foreach ($jiaoshi as $key => $val) {
-			
-			$objSheet->setCellValue("A".$j,$val['ROOM']);
-				
+			$objSheet->setCellValue("A".$j,$val['ROOM']);//填充第一列教室号
+			$jshvalue[]=$val['ROOM'];//将第一列教室号放进数组	
 			$j++;
+		}	
+		//print_r($jshvalue);
+
+		foreach ($data as $key => $val) {
+			for ($h=0;$h<150; $h++) { //
+				for ($k=0; $k <50 ; $k++) { 
+					if($data[$h][ROOM]==$jshvalue[$k]){//判断行
+						if ($data[$h][ROOM]=="1201") {
+							$m=2;
+						}else if ($data[$h][ROOM]=="1202") {
+							$m=3;
+						}else if ($data[$h][ROOM]=="1203") {
+							$m=4;
+						}else if ($data[$h][ROOM]=="1301") {
+							$m=5;
+						}else if ($data[$h][ROOM]=="1302") {
+							$m=6;
+						}else if ($data[$h][ROOM]=="1303") {
+							$m=7;
+						}
+						
+						if($data[$h][JIE]=="1-2"){
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("B".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="1-3") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("B".$m,$data[$h][XINGMING].'3');
+						}else if ($data[$h][JIE]=="1-4") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("B".$m,$data[$h][XINGMING]);
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("C".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="7-10") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("D".$m,$data[$h][XINGMING]);
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("E".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="3-4") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("C".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="7-8") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("D".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="7-9") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("D".$m,$data[$h][XINGMING].'3');
+						}else if ($data[$h][JIE]=="9-10") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("E".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="11-12") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("F".$m,$data[$h][XINGMING]);
+						}else if ($data[$h][JIE]=="11-13") {
+							$objPHPExcel->setActiveSheetIndex($i-1)->setCellValue("F".$m,$data[$h][XINGMING].'3');
+						}
+						
+					}
+
+				}
+			}
+			
 		}
- 	
-		// foreach ($data as $key => $val) {
-			
-		// 	for ($i=0;; $i++) { 
-		// 		if($data[$i][ROOM]==)
-				
-		// 		print_r($data[$i][ROOM]);echo '<br>';
-		// 	}
-			//die;
-			//$objSheet->setCellValue("A".$j,$val['ID']);
-				
-			
-		//}
 
 	}
+
+		//删除MySQL中kebiao数据库
+		$conn = mysqli_connect($servername,$username,$password,$dbname);
+		mysqli_set_charset($conn,'utf8_general_ci');//不写这句话的话会造成乱码
+			if(!$conn){
+				die("连接失败：".mysqli_connect_error());
+			}
+		
+			$sql = "DROP DATABASE kebiao";
+			if(!(mysqli_query($conn,$sql))){
+				echo "数据表删除失败:" . mysqli_error($conn);
+			}
+		
+// 设置活动单指数到第一个表,所以Excel打开这是第一个表
+$objPHPExcel->setActiveSheetIndex(0);		
+
+
+	// //生成excel文件
+	// $objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel5');
+	// // //保存文件
+	// $objWriter->save($dir."/export_1.xls");
 	
-	$objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel5');//生成excel文件
-	$objWriter->save($dir."/export_1.xls");//保存文件
-	// $date = date("Y_m_d",time());
-	// $fileName = "{$date}.xlsx";
+$fileName = "123.xlsx";
+
+// 将输出重定向到一个客户端web浏览器(Excel2007)
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename='.$fileName);
+header('Cache-Control: max-age=0');
+
+//要是输出为Excel2007,使用 Excel2007对应的类，生成的文件名为.xlsx.如果是Excel2005,使用Excel5,对应生成.xls文件
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+// $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+
+//支持浏览器下载生成的文档
+$objWriter->save('php://output');
 
 ?>
